437d7de1f9695e7074858b8820f37768684a32b1
Marvin Audisho <maaud0659@stud.kea.dk>
2022-12-13 12:57:09 +0100

    changes

---
+++ script.js
@@ -0,0 +1,7 @@
+//window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("loaded");
+  document.querySelector(".video").play();
+  document.querySelector(".video").currentTime = 0.7;
+}
